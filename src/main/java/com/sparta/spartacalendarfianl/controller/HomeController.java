756ecf0c67a29d31b89b8bb9e9b238bb9ffcac05
whlsls3377 <whlsls3377@naver.com>
2024-10-04 09:15:29 +0900

    내일배움캠프 Spring 마지막 변경점

---
+++ src/main/java/com/sparta/spartacalendarfianl/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.sparta.spartacalendarfianl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    // 홈 페이지 반환
+    @GetMapping("/")
+    public String index() {
+        return "index";  // templates 폴더의 index.html을 반환
+    }
+
+    // 회원가입 페이지 반환
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";  // templates/signup.html을 반환
+    }
+}
